113847ba4e5fdb196f757024160b54404eebd09a
peder2911 <pglandsverk@gmail.com>
2022-02-02 13:49:34 +0100

    Document DataPartitioner

--- views_partitioning/data_partitioner.py
+++ views_partitioning/data_partitioner.py
@@ -11,6 +11,29 @@ PartitionsDicts = NestedDicts[Tuple[int,int]]
 gtzero = lambda x: x if x > 0 else 0
 
 class DataPartitioner():
+    """
+    DataPartitioner
+    ===============
+
+    parameters:
+        partitions (Union[Dict[str,Dict[str,[Tuple[int,in]]]], views_schema.Partitions])
+
+    A DataPartitioner is a class used to describe and perform time-subsetting
+    of doubly-indexed pandas dataframes (Time-Unit). The class also has several
+    methods that can be used to manipulate the partitioning definition, like
+    padding or trimming the partition sizes, and shifting them left and right.
+
+    The DataPartitioner is callable, and is used like this:
+
+    example:
+        partitioner = DataPartitioner({"A":{"train":(1,100)}})
+        partitioner("A","train",dataset)
+
+    For more information about the time-partitioning scheme used by the views
+    team, see this page:
+
+    https://github.com/prio-data/viewser/wiki/TimePartitioning
+    """
 
     def __init__(self, partitions: Union[PartitionsDicts, views_schema.Partitions]):
         if isinstance(partitions, dict):
@@ -34,30 +57,100 @@ class DataPartitioner():
         sub_from_end = abs(size) if size < 0 else 0
         return self._map(lambda s,e: (s + add_to_start, e - sub_from_end))
 
-    def trim(self, size):
+    def trim(self, size: int):
+        """
+        trim
+        ====
+
+        parameters:
+            size (int)
+
+        Trim the right-hand side of the partitions by size steps.
+        """
         return self._trim(-gtzero(size))
 
     def ltrim(self, size):
+        """
+        trim
+        ====
+
+        parameters:
+            size (int)
+
+        Trim the left-hand side of the partitions by size steps.
+        """
         return self._trim(gtzero(size))
 
     def pad(self, size: int):
+        """
+        trim
+        ====
+
+        parameters:
+            size (int)
+
+        Grow the right-hand side of the partitions by size steps.
+        """
         size = size if size > 0 else 0
         return self._pad(size)
 
     def lpad(self, size: int):
+        """
+        trim
+        ====
+
+        parameters:
+            size (int)
+
+        Grow the left-hand side of the partitions by size steps.
+        """
         size = size if size > 0 else 0
         return self._pad(-size)
 
     def no_overlap(self, rev:bool = False):
+        """
+        no_overlap
+        ==========
+
+        parameters:
+            rev (bool)
+
+        Ensure that there is no overlap between the contained partitions.
+        """
         return self._pmap(lambda p: p.no_overlap(rev = rev))
 
-    def in_extent(self, start, end):
+    def in_extent(self, start: int, end: int):
+        """
+        in_extent
+        =========
+
+        parameters:
+            start (int)
+            end (int)
+
+        Ensure that partitions are within the extent start --- end
+        """
         return self._map(lambda s,e: (s if s > start else start, e if e < end else end))
 
-    def extent(self):
+    def extent(self) -> views_schema.TimeSpan:
+        """
+        extent
+        ======
+
+        Return the extent of the contained partitions as a views_schema.partitioning.TimeSpan.
+        """
         return self.partitions.extent()
 
     def shift_left(self, size: int) -> 'DataPartitioner':
+        """
+        shift_left
+        ==========
+
+        parameters:
+            size (int)
+
+        Shifts the contained partitions size steps to the left.
+        """
         start,end = self.extent()
         return (self
                 .lpad(size)
@@ -65,6 +158,15 @@ class DataPartitioner():
                 .in_extent(start, end))
 
     def shift_right(self, size: int)-> 'DataPartitioner':
+        """
+        shift_left
+        ==========
+
+        parameters:
+            size (int)
+
+        Shifts the contained partitions size steps to the right.
+        """
         start,end = self.extent()
         return (self
                 .pad(size)
